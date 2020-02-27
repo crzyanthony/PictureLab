31f40432a716fef5ccc98ef5640ead2d6ff25683
crzyanthony <44053390+crzyanthony@users.noreply.github.com>
2020-02-27 14:57:09 -0800

    MORE EXERCISES

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -117,6 +117,37 @@ public class IntArrayWorker
       
       
     }
-      
+  public int getLargest(){
+        int largest = matrix[0][0];
+            for(int row = 0; row<matrix.length; row++)
+            {
+                for(int col = 0; col<matrix[0].length; col++)
+                {
+                    if(matrix[row][col]>largest){
+                        {
+                            largest = matrix[row][col];
+                        }
+                    }   
+                }   
+        
+            }
+      return largest;
+    }
+  public int getColTotal(int n)
+  {
+      int total = 0;
+      for(int row = 0; row<matrix.length; row++)
+      {
+          for(int col = 0; col<matrix[0].length; col++)
+          {
+              if(col == n){
+                  total+=matrix[row][col];
+            
+            }
+        }
+    
+    }
+    return total;
  
+}
 }
